5e8a7b9cadcd88ceb6baa877abb966a8c3b2842d
zmw <1713634711@qq.com>
2024-04-26 13:19:16 +0800

    🎉 init(all):

---
+++ src/pages/swipeCell/index.vue
@@ -0,0 +1,17 @@
+<script setup lang=ts>
+function click() {
+  alert('1')
+}
+</script>
+
+<template>
+  <van-swipe-cell @click="click">
+    <template #left>
+      <van-button square type="primary" text="选择" />
+    </template>   <van-cell :border="false" :title="1" value="内容" /> <template #right>
+      <van-button square type="danger" text="删除" />
+    </template>
+  </van-swipe-cell>
+</template>
+
+<style lang="scss" scoped></style>
